42bf20fa825adeff5c4262332633cf2e6b5eda24
astridbos <astrid.bos@wur.nl>
2015-01-12 12:41:59 +0100

    init

---
+++ main.R
@@ -0,0 +1,36 @@
+# AD-Scripting, Astrid Bos & David Scholte-Albers
+# 12 January 2014
+# Exercise 6
+
+# Start with empty environment
+rm(list=ls())
+
+# Import packages
+library(sp)
+library(rgdal)
+library(rgeos)
+
+# Check working directory
+getwd()
+
+# Call functions
+source()
+#source()
+
+# Specify weblinks to download
+#URL
+
+#Download & unzip data
+
+# Change projection
+#project_RD <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889
+#                  +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.2369,
+#                  50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,
+#                  4.0812 +units=m +no_defs")
+
+#railways_RD <- spTransform(railways, project_RD)
+#places_RD <- spTransform(places, project_RD)
+
+# Plot & print result
+
+############ end of script ####################
